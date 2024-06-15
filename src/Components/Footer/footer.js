35577150f116913a7ff92b95dd0848e87d9475d3
Taranveer Flora <71133941+Taranveer1@users.noreply.github.com>
2024-06-15 16:43:18 -0400

    Updated Footer to match the theme

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer className="backgroundColor: '#0a192f' text-blue-200 text-center py-5">
-            <p>&copy; 2024 Coded and Deployed by Taranveer Flora</p>
+        <footer className="backgroundColor: '#0a192f' text-green text-center py-5">
+            <p>&copy; 2024 Developed and Deployed by Taranveer Flora</p>
         </footer>
     );
 }
--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Resume() {
-  return <div className="min-h-screen bg-white">Resume</div>;
+  return <div className="min-h-screen bg-[#0a192f]">Resume</div>;
 }
 
 export default Resume;
